1760bd8d3f3f2842341a6866e8006146603f0ebe
pGUPT4 <pratbro1@gmail.com>
2025-04-04 10:51:08 +0530

    deploy debug 8

--- nextjs-flask/next.config.js
+++ nextjs-flask/next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5000/api/:path*'
-            : '/api/:path*',
+            ? 'http://127.0.0.1:5000/:path*'
+            : '/api/',
       },
     ];
   },
