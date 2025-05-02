4433bbd229682763cad1238b25602f32e032d028
pGUPT4 <pratbro1@gmail.com>
2025-05-02 10:06:30 +0530

    fixed typos

--- components/news/feedTray.tsx
+++ components/news/feedTray.tsx
@@ -16,13 +16,13 @@ const FeedTray: React.FC = () => {
           <p className="text-center text-gray-800">No news available.</p>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {news.map((item, index) => (
+            {/* {news.map((item, index) => (
               <NewsTile
                 key={index} 
                 title={item.title || 'No title'} 
                 url={item.url || '#'} 
               />
-            ))}
+            ))} */}
           </div>
         )}
       </div>
