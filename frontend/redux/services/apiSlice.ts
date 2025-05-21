701069473980265549f74b1d5683795c97001931
pGUPT4 <pratbro1@gmail.com>
2025-05-21 06:25:56 +0530

    update fetch call

--- frontend/redux/services/apiSlice.ts
+++ frontend/redux/services/apiSlice.ts
@@ -1,7 +1,9 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 
-const baseQuery = fetchBaseQuery({ baseUrl: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/` });
+const baseQuery = fetchBaseQuery({ 
+    baseUrl: process.env.NODE_ENV === 'development' ? `${process.env.NEXT_PUBLIC_SERVER_URL}/api/` : '/api/' 
+});
 
 export const apiSlice = createApi({
     baseQuery,
